45a314ae2d918d5c647e2387ffff2cd398b6500b
Glauber Santana <glauber.silva14@gmail.com>
2022-12-07 22:24:34 -0300

    add: schemas and models

---
+++ server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/sglauber/studiosol/graph"
+)
+
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/graphql", playground.Handler("StudioSol - GraphiQL", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/graphql for StudioSol - GraphiQL", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
